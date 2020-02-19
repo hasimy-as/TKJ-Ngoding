6fd1c5d5390e743e38583d208e9c44146e08e0fc
hasimy-as <ahmadhasyimi674@gmail.com>
2020-02-19 14:55:48 +0700

    commiting

---
+++ Basic-Nodejs/EJS-Nodejs/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index.js')
+});
